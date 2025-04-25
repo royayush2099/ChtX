6108b74350439fca86527aeebd74196f88e207ce
Ayushroy <royayush2099@gmail.com>
2025-04-25 20:58:37 +0530

    updated cloudinary.js file in lib

--- src/index.js
+++ src/index.js
@@ -2,13 +2,19 @@ import express from 'express'
 import authRoute from "./routes/auth.route.js"
 import messageRoutes from "./routes/message.route.js"
 import dotenv from 'dotenv'
+dotenv.config()
+import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import {connectDB} from "./lib/db.js"
-dotenv.config()
+
 const app = express();
 const PORT = process.env.PORT
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors({
+    origin:'http://localhost:5173',
+    credentials:true,
+}))
 
 app.use('/api/auth',authRoute)
 app.use('/api/message',messageRoutes);
